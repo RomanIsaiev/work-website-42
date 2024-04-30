60a11d4afc5dfaffa1be26c6bbe32609763b8e73
Roman Isaiev <107892291+RomanIsaiev@users.noreply.github.com>
2024-04-30 19:22:59 +0300

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default () => {
           optimizationLevel: 7,
         },
         mozjpeg: {
-          quality: 20,
+          quality: 80,
         },
         pngquant: {
           quality: [0.8, 0.9],
